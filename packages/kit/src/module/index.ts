d91cf9ca18db5e3ee33a062f74704835932e50f6
Jonathan Moutier <djo.moutier@gmail.com>
2026-04-07 00:29:31 +0200

    feat(kit): add @gwenjs/kit/module entry point

---
+++ packages/kit/src/module/index.ts
@@ -0,0 +1,9 @@
+// packages/kit/src/module/index.ts
+export { defineGwenModule } from '../define-module.js';
+export type {
+  GwenModule,
+  GwenModuleDefinition,
+  GwenKit,
+  GwenBuildHooks,
+  GwenBaseConfig,
+} from '../define-module.js';
